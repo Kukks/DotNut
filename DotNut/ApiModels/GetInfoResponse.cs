ccd53d78a2472bb30450d6f66ca3bf33ce263402
Kukks <evilkukka@gmail.com>
2024-10-08 11:01:09 +0200

    update info response model

--- DotNut/ApiModels/GetInfoResponse.cs
+++ DotNut/ApiModels/GetInfoResponse.cs
@@ -1,18 +1,52 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using DotNut.JsonConverters;
 
 namespace DotNut.ApiModels;
 
 public class GetInfoResponse
 {
-    [JsonPropertyName("name")] public string Name { get; set; }
-    [JsonPropertyName("pubkey")] public string Pubkey { get; set; }
-    [JsonPropertyName("version")] public string Version { get; set; }
-    [JsonPropertyName("description")] public string Description { get; set; }
-    [JsonPropertyName("description_long")] public string DescriptionLong { get; set; }
-    [JsonPropertyName("contact")] public List<ContactInfo> Contact { get; set; }
-    [JsonPropertyName("motd")] public string Motd { get; set; }
-    [JsonPropertyName("nuts")] public Dictionary<string, JsonDocument> Nuts { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("pubkey")]
+    public string? Pubkey { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("version")]
+    public string? Version { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("description_long")]
+    public string? DescriptionLong { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("contact")]
+    public List<ContactInfo>? Contact { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("motd")]
+    public string? Motd { get; set; }
+
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("icon_url")]
+    public string? IconUrl { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonConverter(typeof(UnixDateTimeOffsetConverter))]
+    [JsonPropertyName("time")]
+    public DateTimeOffset? Time { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [JsonPropertyName("nuts")]
+    public Dictionary<string, JsonDocument>? Nuts { get; set; }
 }
 
 public class ContactInfo

26f2986c19ab1006b4ec1f0642b6363280eb3f83
Kukks <evilkukka@gmail.com>
2024-10-08 08:54:02 +0200

    add fee to keyset

--- DotNut/ApiModels/GetKeysetsResponse.cs
+++ DotNut/ApiModels/GetKeysetsResponse.cs
@@ -11,5 +11,7 @@ public class GetKeysetsResponse
         [JsonPropertyName("id")] public KeysetId Id { get; set; }
         [JsonPropertyName("unit")] public string Unit { get; set; }
         [JsonPropertyName("active")] public bool Active { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("input_fee_ppk")] public int? InputFee { get; set; }
     }
 }
---
+++ DotNut/FeeHelper.cs
@@ -0,0 +1,21 @@
+﻿using DotNut.ApiModels;
+
+namespace DotNut;
+
+public static class FeeHelper
+{
+
+    public static int ComputeFee(this IEnumerable<Proof> proofsToSpend, Dictionary<KeysetId, int> keysetFees)
+    {
+        var sum = 0;
+        foreach (var proof in proofsToSpend)
+        {
+            if (keysetFees.TryGetValue(proof.Id, out var fee))
+            {
+                sum += fee;
+            }
+        }
+
+        return (sum + 999) / 1000;
+    }
+}

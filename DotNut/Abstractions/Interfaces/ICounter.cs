ff56229e28a7e058f7f52d4998770c9ca1923a62
d4rp4t <d4rp4t@tutanota.com>
2025-12-07 08:05:50 +0100

    add counter output for restore

--- DotNut/Abstractions/Interfaces/ICounter.cs
+++ DotNut/Abstractions/Interfaces/ICounter.cs
@@ -5,4 +5,5 @@ public interface ICounter
     public Task<int> GetCounterForId(KeysetId keysetId, CancellationToken ct = default);
     public Task<int> IncrementCounter(KeysetId keysetId, int bumpBy = 1, CancellationToken ct = default);
     public Task SetCounter(KeysetId keysetId, int counter, CancellationToken ct = default);
+    public IReadOnlyDictionary<KeysetId, int> Export();
 }
--- DotNut/Abstractions/Interfaces/IRestoreBuilder.cs
+++ DotNut/Abstractions/Interfaces/IRestoreBuilder.cs
@@ -5,6 +5,21 @@ namespace DotNut.Abstractions;
 /// </summary>
 public interface IRestoreBuilder
 {
-    IRestoreBuilder ForKeysetIds(IEnumerable<KeysetId> keysetIds);
+    /// <summary>
+    /// Optional and usually not-advised. Allows to specify keysets that we want to restore.
+    /// If not set, every keyset is grinded.
+    /// </summary>
+    /// <param name="keysetIds"></param>
+    /// <returns></returns>
+    IRestoreBuilder FromKeysetIds(IEnumerable<KeysetId> keysetIds);
+    
+    /// <summary>
+    /// Optional, allows to set counter which will hold the state after restore.
+    /// If not set, defaults to InMemoryCounter that is not returned.
+    /// </summary>
+    /// <param name="counter"></param>
+    /// <returns></returns>
+    public IRestoreBuilder WithCounter(ICounter counter);
+    
     Task<IEnumerable<Proof>> ProcessAsync(CancellationToken ct = default);
 }

90d5427e8327c832673d96be5dba921d8553d61d
d4rp4t <d4rp4t@tutanota.com>
2025-12-22 16:23:54 +0100

    jsonignore on memo

--- DotNut/NUT18/PaymentRequestPayload.cs
+++ DotNut/NUT18/PaymentRequestPayload.cs
@@ -5,6 +5,7 @@ namespace DotNut;
 public class PaymentRequestPayload
 {
     [JsonPropertyName("id")] public string PaymentId { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("memo")] public string? Memo { get; set; }
     [JsonPropertyName("mint")] public string Mint { get; set; }
     [JsonPropertyName("unit")] public string Unit { get; set; }

339fd07239bd92d563a21cce5805c0b2a66e5206
d4rp4t <d4rp4t@tutanota.com>
2026-01-12 14:56:03 +0100

    Add "active" flag "input_fee_ppk" to GetKeysResponse

--- DotNut/ApiModels/GetKeysResponse.cs
+++ DotNut/ApiModels/GetKeysResponse.cs
@@ -10,6 +10,9 @@ public class GetKeysResponse
     {
         [JsonPropertyName("id")] public KeysetId Id { get; set; }
         [JsonPropertyName("unit")] public string Unit { get; set; }
+        [JsonPropertyName("active")] public bool? Active { get; set; } // nullable until wider adoption
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("input_fee_ppk")] public ulong? InputFeePpk { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("final_expiry")] public ulong? FinalExpiry { get; set; }
         [JsonPropertyName("keys")] public Keyset Keys { get; set; }

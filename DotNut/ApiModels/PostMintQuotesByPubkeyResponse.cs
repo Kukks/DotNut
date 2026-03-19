dea3e5af26f0537d5471136048a0b7b0ada5961c
d4rp4t <d4rp4t@tutanota.com>
2026-03-19 20:07:02 +0100

    add jsonconverter annotation

--- DotNut/ApiModels/PostMintQuotesByPubkeyResponse.cs
+++ DotNut/ApiModels/PostMintQuotesByPubkeyResponse.cs
@@ -1,7 +1,9 @@
+using System.Text.Json.Serialization;
 using DotNut.ApiModels.Mint.bolt12;
 
 namespace DotNut.ApiModels;
 
+[JsonConverter(typeof(PostMintQuotesByPubkeyResponseConverter))]
 public class PostMintQuotesByPubkeyResponse
 {
     public PostMintQuoteBolt11Response[] Bolt11Quotes { get; set; }
